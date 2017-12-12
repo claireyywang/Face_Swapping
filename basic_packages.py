089de597ffed3f57fb8382826521590ad35a6b47
Hsieh Yuho <yuho.yhhsieh@gmail.com>
2017-12-12 17:28:51 -0500

    basic packages

---
+++ basic_packages.py
@@ -0,0 +1,15 @@
+'''
+File clarification:
+    import basic library such as numpy and so on
+'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from mpl_toolkits.mplot3d import Axes3D  
+from PIL import Image
+import sys, pdb, scipy.misc, os.path
+
+sys.path.append('../Convolutional_Neural_Networks/')
+
+import PyNet as net
