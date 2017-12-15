10ecfd2d269d640590fb01d0bafab0fe0ac51f39
Hsieh Yuho <yuho.yhhsieh@gmail.com>
2017-12-14 22:27:04 -0500

    only include create video

--- runme.py
+++ runme.py
@@ -1,10 +1,10 @@
-from functions import *
+from create_video import create_video
 
 input_path_s = 'input_video/video1.mp4'
 input_path_t = 'input_video/video1.mp4'
 len_frame = 10
 output_path = 'output_video/output.mp4' 
 
-create_video(input_path_s,input_path_t,output_path)
+create_video(input_path_s,input_path_t,10,output_path)
 
 
