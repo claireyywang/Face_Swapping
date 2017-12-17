8a1c1e487bbb22c3ef05bb1cf78af0894ab37986
Hsieh Yuho <yuho.yhhsieh@gmail.com>
2017-12-17 18:04:20 -0500

    add path easy medium

--- runme.py
+++ runme.py
@@ -1,10 +1,17 @@
 from create_video import create_video
 
-input_path_s = 'input_video/Yuho.mp4'
-input_path_t = 'input_video/easy/FrankUnderwood.mp4'
-len_frame = 10
-output_path = 'output_video/output.mp4' 
+input_path_s = 'input_video/medium/LucianoRosso1.mp4'
+input_path_t = 'input_video/medium/LucianoRosso2.mp4'
 
-create_video(input_path_s,input_path_t,10,output_path)
+# input_path_s = 'input_video/easy/MrRobot.mp4'
+# input_path_t = 'input_video/easy/Yuho.mp4'
+
+
+len_frame = 200
+output_path_s = 'output_video/swap_s.avi' 
+output_path_t = 'output_video/swap_t.avi' 
+
+
+create_video(input_path_s,input_path_t,len_frame,output_path_s,output_path_t)
 
 
